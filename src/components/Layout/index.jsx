b10ea45b8e514be84adbd3046e96477a978a7ecc
Le Thanh Tung <iamrobotdiy@gmail.com>
2024-10-28 12:53:32 +0700

    add component footer

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
     <>
-      
       <Outlet />
-      
+      <Footer />
     </>
   );
 };
