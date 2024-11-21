a8bcaab1c66b3da9640505f5892e6f9897860db6
Lại Thanh Xuân <125149598+Xuanlai583@users.noreply.github.com>
2024-11-21 07:36:28 +0700

    add log out

--- src/pages/Auth/logout/logout.jsx
+++ src/pages/Auth/logout/logout.jsx
@@ -1,16 +1,27 @@
-import "./style.css";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Logout() {
-  setTimeout(() => {
-    window.location.href = "/";
-  }, 5000);
+const Logout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Chuyển hướng sau 5 giây
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
-    <div className="exit-succ">
-      <h6>Thoát khỏi hệ thống thành công</h6>
-      <h6>Bạn đã thoát khỏi tài khoản thành công. Hệ thống sẽ chuyển về trang chủ trong 5 giây, bạn vui lòng đợi</h6>
+    <div>
+      <h4>Thoát khỏi hệ thống thành công</h4>
+      <p>
+        Bạn đã thoát khỏi tài khoản thành công. Hệ thống sẽ chuyển về trang chủ
+        trong 5 giây, bạn vui lòng đợi.
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Logout;
